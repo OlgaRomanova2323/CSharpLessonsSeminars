b1f3825d50489f63b99d9b385c32a296c26fce19
Olga Romanova <olgatokach@mail.ru>
2024-05-14 13:49:41 +0300

    добавили код сразу в два файла Task4 и HomeTask

---
+++ Lesson1/HomeTask/Program.cs
@@ -0,0 +1,11 @@
+﻿//комментарий что я молодец
+//  a b c d  это некотрые целые числа. науйдем a*b/(c+d)
+
+float a = 5;
+int b = 7;
+int c = 4;
+int d = 6;
+float formula = a * b / (c + d);
+Console.WriteLine(formula);
+
+
